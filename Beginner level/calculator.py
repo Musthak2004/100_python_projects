0b3f54bc234e609808c0c4565a624330f711ad66
Musthak <musthakcool55@gmail.com>
2026-05-15 22:17:52 +0530

    need to finished this calculator project

---
+++ Beginner level/calculator.py
@@ -0,0 +1,11 @@
+def add(num1, num2):
+    result = num1 + num2
+    return result
+
+def substract(num1, num2):
+    result = num1 - num2
+    return result
+
+def multiply(num1, num2):
+    result = num1 * num2
+    return result
