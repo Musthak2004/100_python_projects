decaf9173b1a96bddd39fba37d1ebbc86135630a
Musthak <musthakcool55@gmail.com>
2026-05-19 19:23:31 +0530

    initial commit

--- Beginner level/rockPaperScissor.py
+++ Beginner level/rockPaperScissor.py
@@ -2,7 +2,7 @@ import random
 
 print("===== Rock Paper Scissors Game =====")
 
-def getUserInput():
+def getUserChoice():
     player = None
 
     while player not in ['r', 'p', 's']:
@@ -15,34 +15,75 @@ def getUserInput():
         player = input("Enter your choice: ").lower()
 
         if player not in ['r', 'p', 's']:
-            print("❌ Invalid choice! Try again.\n")
+            print("Invalid choice! Try again.\n")
 
     return player
 
+
 def getComputerChoice():
     num = random.randint(1, 3)
-    
-    match num:
-        case 1:
-            return 'r'
-        case 2:
-            return 'p'
-        case 3:
-            return 's'
-        
-    return 'r'
+
+    if num == 1:
+        return 'r'
+    elif num == 2:
+        return 'p'
+    else:
+        return 's'
+
 
 def showChoice(choice):
-    match choice:
-        case 'r':
-            print("Rock\n")
-        case 'p':
-            print("Rock\n")
-        case 's':
-            print("Scissors\n")
+    if choice == 'r':
+        print("Rock\n")
+    elif choice == 'p':
+        print("Paper\n")
+    else:
+        print("Scissors\n")
+
 
 def chooseWinner(player, computer):
+
     if player == computer:
         print("It's a tie\n")
 
+    elif player == 'r':
+        if computer == 's':
+            print("YOU WIN!\n")
+        else:
+            print("YOU LOSE!\n")
+
+    elif player == 'p':
+        if computer == 'r':
+            print("YOU WIN!\n")
+        else:
+            print("YOU LOSE!\n")
+
+    elif player == 's':
+        if computer == 'p':
+            print("YOU WIN!\n")
+        else:
+            print("YOU LOSE!\n")
+
+
+# GAME LOOP FIXED
+playAgain = 'y'
+
+while playAgain.lower() == 'y':
+
+    player = getUserChoice()
+
+    print("\nYour choice:")
+    showChoice(player)
+
+    computer = getComputerChoice()
+
+    print("Computer choice:")
+    showChoice(computer)
+
+    chooseWinner(player, computer)
+
+    playAgain = input("Play again? (y/n): ")
+
+    print("\n")
+
+print("Thanks for playing!")
 print("====================================")
