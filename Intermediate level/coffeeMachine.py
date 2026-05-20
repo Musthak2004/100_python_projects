5cd1a4fa555aa9ab28159ada5b8e2f1070648194
Musthak <musthakcool55@gmail.com>
2026-05-20 15:31:24 +0530

    initial commit

---
+++ Intermediate level/coffeeMachine.py
@@ -0,0 +1,10 @@
+print("===== COFFEE MACHINE =====\n")
+
+def userOrder():
+    print("Menu Card:\n")
+    print("1. Espresso")
+    print("---Ingredients: Coffee Beans, Water")
+    print("---Cost: $3.00 - $4.50")
+    print()
+
+userOrder()
